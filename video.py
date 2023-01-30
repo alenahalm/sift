6aba056f87c0b2ea0371b9790ebf5238d8bca17c
alenahalm <75882124+alenahalm@users.noreply.github.com>
2023-01-30 11:21:57 +0800

    Update video.py

--- video.py
+++ video.py
@@ -3,9 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-single = cv.imread("face.png", 0)
-# many = cv.imread("many_cereals.jpg", 0)
+cam = cv.VideoCapture(0)
 
+ret, frame = cam.read()
+r = cv.selectROI("select the area", frame)
+
+single = frame[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
 cam = cv.VideoCapture(0)
 
 while True:
@@ -33,22 +36,15 @@ while True:
         src_pts = np.float32([key_points1[m[0].queryIdx].pt for m in best]).reshape(-1, 1, 2)
         dst_pts = np.float32([key_points2[m[0].trainIdx].pt for m in best]).reshape(-1, 1, 2)
         M, hmask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
-        h, w = single.shape
+        h, w, c = single.shape
         pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
         dst = cv.perspectiveTransform(pts, M)
-        result = cv.polylines(frame, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
-        cv.imshow('res', result)
-        k = cv.waitKey(10)
-        if k > 0:
-            if chr(k) == 'q':
-                break
+        frame = cv.polylines(frame, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
     else:
         print("Not enough matches")
         mask = None
 
     matches_images = cv.drawMatchesKnn(single, key_points1, frame, key_points2, best, None)
-
-    # matches_images = cv.drawMatches(single, key_points1, many, key_points2, best, None)
 
     cv.imshow('frame', frame)
     k = cv.waitKey(10)
